42e8503193f39c513701793eb6247f962ea8af72
Crowdbotics <engineering@crowdbotics.com>
2023-01-24 03:27:54 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gentle_cloud_dev_59047/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gentle_cloud_dev_59047;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gentle_cloud_dev_59047";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-cloud-dev-59047",
+  url: "https://gentle-cloud-dev-59047.botics.co",
+  api: "https://gentle-cloud-dev-59047.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
